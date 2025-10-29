6ccebb054c8ea1691f3ed2f3f4491e0799211eb1
asmith2233 <ayannasmith2233@gmail.com>
2025-10-29 14:23:36 -0400

    creating the file

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class House extends Asset {
+    public House(String mainHouse, String number,
+                 int i, String s, int i1, int i2, int i3) {
+        super();
+    }
+}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Main {
-}
+        public static void main(String[] args) {
+
+
+            ArrayList<Asset> assets = new ArrayList<>();
+
+            Asset jewel = new Asset("Diamond", "June", 10000);
+
+            House h = new House("Main House", "2007", 220000, "123 Smith St", 2, 2500, 10000);
+
+            assets.add(jewel);
+            assets.add(h);
+
+
+            for( Asset a : assets){
+                System.out.println("------------------------------------");
+                System.out.println("Description: " + a.getDescription());
+                System.out.println("Date Acquired: " + a.getDateAcquired());
+                System.out.println("Original Cost: " + a.getOriginalCost());
+                System.out.println("Current Value: " + a.getValue());
+            }
+        }
+    }
